8531fcaab24c425153c02130277245bb30ab00c0
Jagrav Naik <jagravnaik0@gmail.com>
2018-06-01 11:51:44 -0400

    berkeley: POSP bring-up
    Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>

--- potato.mk
+++ potato.mk
@@ -16,10 +16,10 @@
 
 $(call inherit-product, device/huawei/berkeley/full_berkeley.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common POSP stuff.
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_berkeley
+PRODUCT_NAME := potato_berkeley
 PRODUCT_DEVICE := berkeley
 PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := Honor View 10
