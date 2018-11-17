016d604a7d8ab906567994c436401741a5cd7db1
David Sn <divad.nnamtdeis@gmail.com>
2018-11-17 18:05:03 +0100

    berkeley: POSP bring-up for Android Pie

--- potato_berkeley.mk
+++ potato_berkeley.mk
@@ -16,22 +16,26 @@
 
 # Get the long list of APNs
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/potato/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # Inherit AOSP device configuration for berkeley
 $(call inherit-product, device/huawei/berkeley/full_berkeley.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common POSP stuff.
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
 # Disable dynamic partition size
 PRODUCT_USE_DYNAMIC_PARTITION_SIZE := false
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_berkeley
+PRODUCT_NAME := potato_berkeley
 PRODUCT_DEVICE := berkeley
 PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := Honor View 10
 PRODUCT_MANUFACTURER := Huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
+
+# Maintainer prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="David Sn (divadsn)"
